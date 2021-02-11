06aee554fc015653162c25f0793eaf449c5a869f
kazaller <35352091+kazaller@users.noreply.github.com>
2021-02-10 22:25:51 -0800

    Delete src directory

--- src/models/SceneCallback/LaunchOptionsMenuCallback.java
+++
@@ -1,7 +0,0 @@
-package models.SceneCallback;
-
-import java.util.UUID;
-
-public interface LaunchOptionsMenuCallback {
-    public void launchOptionsMenu(UUID playerId);
-}
--- src/models/SceneCallback/LaunchShapePickerCallback.java
+++
@@ -1,7 +0,0 @@
-package models.SceneCallback;
-
-import models.Player;
-
-public interface LaunchShapePickerCallback {
-    public void launchShapePicker(Player player);
-}
