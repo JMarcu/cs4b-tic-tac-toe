db4943e356afc696813441167018625b0d70e774
kazaller <35352091+kazaller@users.noreply.github.com>
2021-02-15 20:57:49 -0800

    added more enums to MarkerShape

--- TicTacToe/src/models/MarkerShape.java
+++ TicTacToe/src/models/MarkerShape.java
@@ -3,7 +3,14 @@ package models;
 public enum MarkerShape {
     CHECKMARK ("checkmark.png"),
     X ("x.png"), 
-    O ("o.png");
+    O ("o.png"),
+    STAR ("Star.png"),
+    FILLED ("ColorFilled.png"),
+    OUTLINE ("OutlineFilled.png"),
+    FROWNY ("FrownyFace.png"),
+    SMILEY ("SmileyFace.png"),
+    DRAGON ("Dragon.png"),
+    CAT ("CatFace.png");
 
     private final String filename;
 
