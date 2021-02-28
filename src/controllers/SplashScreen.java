83c33a9a424869d4e2c8347023ab56cd9f13cf9a
Joshua <joshuaacosta2@gmail.com>
2021-02-27 21:34:20 -0800

    Added splash screen framework

---
+++ src/controllers/SplashScreen.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class SplashScreen {
+    @FXML private ImageView splashImageView;
+    @FXML private Label splashText;
+    @FXML private Button splashButton;
+
+    @FXML
+    private void handleSplashButton(MouseEvent e){
+
+    }
+
+}
