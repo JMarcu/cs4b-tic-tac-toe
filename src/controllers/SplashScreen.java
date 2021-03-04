2e5bc535835468aa02d6b4332582085b20eae913
Joshua <joshuaacosta2@gmail.com>
2021-03-03 21:12:06 -0800

    Splash Screen update

--- src/controllers/SplashScreen.java
+++ src/controllers/SplashScreen.java
@@ -3,8 +3,10 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class SplashScreen {
     @FXML private ImageView splashImageView;
@@ -12,8 +14,16 @@ public class SplashScreen {
     @FXML private Button splashButton;
 
     @FXML
-    private void handleSplashButton(MouseEvent e){
+    private void Initialize() {
+        Image image = new Image("src/assets/images/trophy.png");
+        splashImageView.setImage(image);
+        splashText.setText("YOU WON!");
+    }
 
+    @FXML
+    private void handleSplashButton(MouseEvent e){
+        Stage stage = (Stage) splashButton.getScene().getWindow();
+        stage.close();
     }
 
 }
