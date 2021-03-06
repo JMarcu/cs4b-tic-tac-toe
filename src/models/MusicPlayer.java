c93ac5c34f0a54f1cf9f5a5940e4ced9581c3325
Joshua <joshuaacosta2@gmail.com>
2021-03-05 17:39:37 -0800

    changed tracks to enumType

--- src/models/MusicPlayer.java
+++ src/models/MusicPlayer.java
@@ -3,33 +3,37 @@ package models;
 import java.io.File;
 import javax.sound.sampled.*;
 
+enum Track{
+    adjustSound, changeMarker, clickSquare, exitMenu, openMenu, title, waiting, lose, tie, win;
+}
+
 public class MusicPlayer {
     private final String SOUND_DIRECTORY = "src/assets/sounds/";
 
-    void playMusic(char track){
+    void playMusic(Track track){
         String musicLocation = "";
         switch(track){
-            case '0':   musicLocation = SOUND_DIRECTORY.concat("adjust-sound.wav");
+            case adjustSound:   musicLocation = SOUND_DIRECTORY.concat("adjust-sound.wav");
                 break;
-            case '1':   musicLocation = SOUND_DIRECTORY.concat("change-marker.wav");
+            case changeMarker:  musicLocation = SOUND_DIRECTORY.concat("change-marker.wav");
                 break;
-            case '2':   musicLocation = SOUND_DIRECTORY.concat("click-square.wav");
+            case clickSquare:   musicLocation = SOUND_DIRECTORY.concat("click-square.wav");
                 break;
-            case '3':   musicLocation = SOUND_DIRECTORY.concat("exit-menu.wav");
+            case exitMenu:      musicLocation = SOUND_DIRECTORY.concat("exit-menu.wav");
                 break;
-            case '4':   musicLocation = SOUND_DIRECTORY.concat("open-menu.wav");
+            case openMenu:      musicLocation = SOUND_DIRECTORY.concat("open-menu.wav");
                 break;
-            case '5':   musicLocation = SOUND_DIRECTORY.concat("title-theme.wav");
+            case title:         musicLocation = SOUND_DIRECTORY.concat("title-theme.wav");
                 break;
-            case '6':   musicLocation = SOUND_DIRECTORY.concat("waiting.wav");
+            case waiting:       musicLocation = SOUND_DIRECTORY.concat("waiting.wav");
                 break;
-            case '7':   musicLocation = SOUND_DIRECTORY.concat("you-lose.wav");
+            case lose:          musicLocation = SOUND_DIRECTORY.concat("you-lose.wav");
                 break;
-            case '8':   musicLocation = SOUND_DIRECTORY.concat("you-tied.wav");
+            case tie:           musicLocation = SOUND_DIRECTORY.concat("you-tied.wav");
                 break;
-            case '9':   musicLocation = SOUND_DIRECTORY.concat("you-won.wav");
+            case win:           musicLocation = SOUND_DIRECTORY.concat("you-won.wav");
                 break;
-            default:    musicLocation = SOUND_DIRECTORY.concat("waiting.wav");
+            default:
                 break;
         }//end switch
 
@@ -41,6 +45,9 @@ public class MusicPlayer {
                 clip.open(audioInput);
                 clip.start();
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+            else{
+                System.out.println("Can't find the file");
             }
         } catch(Exception ex){
 
