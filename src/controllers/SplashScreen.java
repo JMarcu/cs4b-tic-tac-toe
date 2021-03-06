cb6349f4fc9f3d17e960e78ad360afde55d528d3
Joshua <joshuaacosta2@gmail.com>
2021-03-05 17:57:01 -0800

    support for different splash screens

--- src/controllers/SplashScreen.java
+++ src/controllers/SplashScreen.java
@@ -8,16 +8,44 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+enum Splash{
+    title, lose, tie, win;
+}
+
 public class SplashScreen {
+    private final String SPLASH_DIRECTORY = "src/assets/images/splash";
+
     @FXML private ImageView splashImageView;
     @FXML private Label splashText;
     @FXML private Button splashButton;
 
     @FXML
     private void Initialize() {
-        Image image = new Image("src/assets/images/splash/trophy.png");
+    }
+
+    @FXML
+    public void showSplash(Splash type){
+        String splashLocation = "";
+        String message = "";
+        switch(type){
+            case title: splashLocation = SPLASH_DIRECTORY.concat("title.png");
+                break;
+            case lose:  splashLocation = SPLASH_DIRECTORY.concat("grave.png");
+                        message = "YOU LOSE";
+                break;
+            case tie:   splashLocation = SPLASH_DIRECTORY.concat("tie.png");
+                        message = "YOU TIED";
+                break;
+            case win:   splashLocation = SPLASH_DIRECTORY.concat("trophy.png");
+                        message = "YOU WON";
+                break;
+            default:
+                break;
+        }
+
+        Image image = new Image(splashLocation);
         splashImageView.setImage(image);
-        splashText.setText("YOU WON!");
+        splashText.setText(message);
     }
 
     @FXML
