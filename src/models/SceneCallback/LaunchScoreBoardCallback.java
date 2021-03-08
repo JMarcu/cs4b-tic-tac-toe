4a31533ea826efaa405f6a9258384f75bff05970
Your Name <you@example.com>
2021-03-07 21:36:48 -0800

    Scoreboard update

--- src/models/SceneCallback/LaunchScoreBoardCallback.java
+++ src/models/SceneCallback/LaunchScoreBoardCallback.java
@@ -1,10 +1,8 @@
 package models.SceneCallback;
 
 import java.util.UUID;
-//import java.time.Instant;
 import models.TTTScene;
 import models.GameState;
-// import models.Player;
 
 public interface LaunchScoreBoardCallback {
     public void launchScoreBoard(UUID playerID, TTTScene returnTo, GameState gameState);
