d9b39cc364adb01d1f4a4a5e8c5537629416146c
Sarah Whitelaw <sarah.rose.whitelaw@gmail.com>
2021-03-07 21:38:37 -0800

    Added sound effects for opening and closing shape / options menus (and for clicking some of the buttons).

--- src/controllers/OptionsController.java
+++ src/controllers/OptionsController.java
@@ -5,27 +5,39 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
 import models.SceneCallback.ReturnToCallback;
 import models.SceneCallback.LaunchMainMenuCallback;
+import models.MusicPlayer;
 
 public class OptionsController {
 
     private LaunchMainMenuCallback mainMenuCB;
     private ReturnToCallback returnToCB;
+    //private MusicPlayer music;
 
     @FXML private GridPane root;
 
     public GridPane getRoot(){ return this.root; }
-
+    
     @FXML protected void ReturnButtonClicked(ActionEvent event) {
+        MusicPlayer music = new MusicPlayer();
+        music.playSFX(MusicPlayer.Track.exitMenu);
         returnToCB.returnTo();
     }
 
     @FXML protected void SFXButtonClicked(ActionEvent event) {
         // changes sound effects volume
+
+        //play sound effect to show effect
+        MusicPlayer music = new MusicPlayer();
+        music.playSFX(MusicPlayer.Track.adjustSound);
     }
 
     @FXML protected void VolumeButtonClicked(ActionEvent event) {
         // Lets you change the volume
         // CycleVolume();
+
+        //play sound effect to show effect
+        MusicPlayer music = new MusicPlayer();
+        music.playSFX(MusicPlayer.Track.adjustSound);
     }
 
     @FXML protected void RestartButtonClicked(ActionEvent event) {
--- src/controllers/ShapeColorController.java
+++ src/controllers/ShapeColorController.java
@@ -9,6 +9,7 @@ import models.ColorScheme;
 import models.MarkerShape;
 import models.Player;
 import models.SceneCallback.ReturnToCallback;
+import models.MusicPlayer;
 
 public class ShapeColorController {
     @FXML
@@ -21,6 +22,7 @@ public class ShapeColorController {
     private MarkerShape myShape;
     private Player player;
     private ReturnToCallback returnCB;
+    private MusicPlayer musicSFX;
 
     public void acceptPlayer(Player player)
     {
@@ -29,12 +31,15 @@ public class ShapeColorController {
         myShape = player.getShape();
         this.player = player;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX = new MusicPlayer();
     }
 
     public SplitPane getRoot(){ return this.root; }
 
     @FXML protected void ReturnButtonClicked(ActionEvent event) {
-        System.out.println("currentColor: " + currentColor);
+        //System.out.println("currentColor: " + currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.exitMenu);
+
         player.setColor(currentColor != null ? currentColor : Color.BLACK);
         player.setShape(myShape);
         returnCB.returnTo();
@@ -44,99 +49,117 @@ public class ShapeColorController {
         Image image = new Image("assets/images/x.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.X;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void OButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/o.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.O;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void SmileyButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/SmileyFace.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.SMILEY;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void FrownyButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/FrownyFace.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.FROWNY;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void ColorFilledButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/ColorFilled.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.FILLED;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void OutlineFilledButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/OutlineFilled.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.OUTLINE;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void DragonButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/Dragon.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.DRAGON;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void CatButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/CatFace.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.CAT;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void StarButtonClicked(ActionEvent event) {
         Image image = new Image("assets/images/Star.png");
         CurrentPlayerImage.setImage(image);
         myShape = MarkerShape.STAR;
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void YellowButtonClicked(ActionEvent event) {
         currentColor = Color.YELLOW;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void RedButtonClicked(ActionEvent event) {
         currentColor = Color.RED;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void OrangeButtonClicked(ActionEvent event) {
         currentColor = Color.ORANGE;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void PinkButtonClicked(ActionEvent event) {
         currentColor = Color.PINK;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void BlueButtonClicked(ActionEvent event) {
         currentColor = Color.BLUE;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void GreenButtonClicked(ActionEvent event) {
         currentColor = Color.GREEN;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void TealButtonClicked(ActionEvent event) {
         currentColor = Color.TEAL;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void PurpleButtonClicked(ActionEvent event) {
         currentColor = Color.PURPLE;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     @FXML protected void BlackButtonClicked(ActionEvent event) {
         currentColor = Color.BLACK;
         ColorScheme.adjustImageColor(CurrentPlayerImage, currentColor);
+        musicSFX.playSFX(MusicPlayer.Track.changeMarker);
     }
 
     public void setReturnCB(ReturnToCallback returnCB){
