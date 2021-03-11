3071d74ef27db01b2d48f29be6895be6b55797b3
JMarcu <james.marcu@yahoo.com>
2021-03-10 18:20:20 -0800

    Fixed javadoc script and docs missing root level models.

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controllers"},{"l":"models.SceneCallback"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controllers"},{"l":"models"},{"l":"models.SceneCallback"}]
