01696991c8c9baea098b6255ed1a6ba2a36760ef
Joshua <joshuaacosta2@gmail.com>
2021-04-19 18:38:16 -0700

    login and registration screen baseline

---
+++ src/controllers/Login.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class Login {
+    @FXML private TextField usernameField;
+    @FXML private TextField passwordField;
+    @FXML private Button loginBtn;
+    @FXML private Button registerBtn;
+    @FXML private Pane root;
+}
---
+++ src/controllers/Register.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class Register {
+    @FXML private TextField usernameField;
+    @FXML private TextField passwordField;
+    @FXML private Button registerBtn;
+    @FXML private Pane root;
+}
